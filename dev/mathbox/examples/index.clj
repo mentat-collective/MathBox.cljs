02818cc9dc9c4de16a5c8223147650420cfca36e
Sam Ritchie <sritchie09@gmail.com>
2023-01-25 16:34:29 -0700

    fix examples index

---
+++ dev/mathbox/examples/index.clj
@@ -0,0 +1,123 @@
+^#:nextjournal.clerk
+{:toc true
+ :visibility :hide-ns}
+(ns mathbox.examples.index)
+
+;; # Examples Index
+
+;; This directory contains all demos of MathBox. Most are not yet implemented,
+;; but we're working on it!
+
+;; Each example below is marked with one of the these three statuses:
+
+;; - ✅ Done!
+;; - 🚧 Under Construction: both links should work, but the example is not yet polished.
+;; - ❌ Not yet started: These links won't work.
+
+;; ## Math
+
+;; - ❌ [Color Cube](math/colorcube.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/math/colorcube.clj))
+;; - ❌ [Exponential](math/exponential.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/math/exponential.clj))
+;; - ❌ [Field Lines](math/fieldlines.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/math/fieldlines.clj))
+;; - ❌ [Helitorus](math/helitorus.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/math/helitorus.clj))
+;; - ❌ [Hopf](math/hopf.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/math/hopf.clj))
+;; - ❌ [Hyperbolic](math/hyperbolic.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/math/hyperbolic.clj))
+;; - ❌ [Ortho](math/ortho.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/math/ortho.clj))
+;; - ❌ [Procedural](math/procedural.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/math/procedural.clj))
+;; - ❌ [Quat](math/quat.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/math/quat.clj))
+;; - ❌ [Tiling](math/tiling.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/math/tiling.clj))
+;; - ❌ [Water](math/water.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/math/water.clj))
+
+;; ## Science
+
+;; - ❌ [Solar System](sci/solarsystem.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/sci/solarsystem.clj))
+
+;; ## Dataviz
+
+;; - 🚧 [Scatter Plot](dataviz/scatter.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/dataviz/scatter.clj))
+
+;; ## Demo
+
+;; - ❌ [Audio Visualizer](demo/audio-visualizer.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/demo/audio-visualizer.clj))
+;; - ❌ [Cylindrical Stream](demo/cylindrical-stream.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/demo/cylindrical-stream.clj))
+;; - ❌ [Cylindrical](demo/cylindrical.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/demo/cylindrical.clj))
+;; - ❌ [Labels](demo/labels.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/demo/labels.clj))
+;; - ❌ [Piano](demo/piano.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/demo/piano.clj))
+;; - ❌ [Shapes](demo/shapes.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/demo/shapes.clj))
+
+;; ## Test
+
+;; - ❌ [Axis](test/axis.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/axis.clj))
+;; - ❌ [Camera](test/camera.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/camera.clj))
+;; - ❌ [Cartesian4](test/cartesian4.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/cartesian4.clj))
+;; - ❌ [Closed Line](test/closed-line.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/closed-line.clj))
+;; - ❌ [Closed Surface](test/closed-surface.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/closed-surface.clj))
+;; - ❌ [Compose](test/compose.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/compose.clj))
+;; - ❌ [Context](test/context.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/context.clj))
+;; - ❌ [Curve](test/curve.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/curve.clj))
+;; - ❌ [Curve Dots](test/curvedots.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/curvedots.clj))
+;; - ❌ [Data](test/data.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/data.clj))
+;; - ❌ [Disc](test/disc.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/disc.clj))
+;; - ❌ [DOM Clone](test/dom-clone.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/dom-clone.clj))
+;; - ❌ [DOM Latex](test/dom-latex.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/dom-latex.clj))
+;; - ❌ [Dom VDOM](test/dom-vdom.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/dom-vdom.clj))
+;; - ❌ [Face](test/face.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/face.clj))
+;; - ❌ [Feedback](test/feedback.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/feedback.clj))
+;; - ❌ [Format](test/format.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/format.clj))
+;; - ❌ [Fragment Color](test/fragmentcolor.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/fragmentcolor.clj))
+;; - ❌ [Grid](test/grid.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/grid.clj))
+;; - ❌ [Grow](test/grow.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/grow.clj))
+;; - ❌ [Helix](test/helix.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/helix.clj))
+;; - ❌ [History](test/history.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/history.clj))
+;; - ❌ [Join](test/join.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/join.clj))
+;; - ❌ [Join Split](test/joinsplit.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/joinsplit.clj))
+;; - ❌ [Label](test/label.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/label.clj))
+;; - ❌ [Label2](test/label2.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/label2.clj))
+;; - ❌ [Lerp](test/lerp.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/lerp.clj))
+;; - ❌ [Line](test/line.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/line.clj))
+;; - ❌ [Mask](test/mask.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/mask.clj))
+;; - ❌ [Memo](test/memo.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/memo.clj))
+;; - ❌ [Network](test/network.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/network.clj))
+;; - ❌ [Partial Data](test/partial-data.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/partial-data.clj))
+;; - ❌ [Play](test/play.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/play.clj))
+;; - ❌ [Point](test/point.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/point.clj))
+;; - ❌ [Point Cloud](test/pointcloud.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/pointcloud.clj))
+;; - ❌ [Point Sizes](test/pointsizes.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/pointsizes.clj))
+;; - ❌ [Polar](test/polar.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/polar.clj))
+;; - ❌ [Present](test/present.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/present.clj))
+;; - ❌ [Present2](test/present2.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/present2.clj))
+;; - ❌ [Readback](test/readback.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/readback.clj))
+;; - ❌ [Repeat](test/repeat.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/repeat.clj))
+;; - ❌ [Resample](test/resample.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/resample.clj))
+;; - ❌ [Resample2](test/resample2.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/resample2.clj))
+;; - ❌ [RTT](test/rtt.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/rtt.clj))
+;; - ❌ [Scale](test/scale.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/scale.clj))
+;; - ❌ [Scatter](test/scatter.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/scatter.clj))
+;; - ❌ [Shader](test/shader.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/shader.clj))
+;; - ❌ [Sources](test/sources.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/sources.clj))
+;; - ❌ [Spherical](test/spherical.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/spherical.clj))
+;; - ❌ [Split](test/split.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/split.clj))
+;; - ❌ [Spread](test/spread.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/spread.clj))
+;; - ❌ [Stereographic](test/stereographic.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/stereographic.clj))
+;; - ❌ [Stereographic4](test/stereographic4.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/stereographic4.clj))
+;; - ❌ [Strip](test/strip.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/strip.clj))
+;; - ❌ [Subdivide](test/subdivide.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/subdivide.clj))
+;; - ❌ [Surface](test/surface.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/surface.clj))
+;; - ❌ [ThreeJS](test/threejs.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/threejs.clj))
+;; - ❌ [Ticks](test/ticks.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/ticks.clj))
+;; - ❌ [Transition](test/transition.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/transition.clj))
+;; - ❌ [Transpose](test/transpose.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/transpose.clj))
+;; - ❌ [Vector](test/vector.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/vector.clj))
+;; - ❌ [Vertex](test/vertex.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/vertex.clj))
+;; - ❌ [Vertex Color](test/vertexcolor.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/vertexcolor.clj))
+;; - ❌ [Vertex Feedback](test/vertexfeedback.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/vertexfeedback.clj))
+;; - ❌ [Visible](test/visible.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/visible.clj))
+;; - ❌ [Volume](test/volume.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/volume.clj))
+;; - ❌ [World](test/world.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/world.clj))
+;; - ❌ [xyzw](test/xyzw.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/xyzw.clj))
+;; - ❌ [Z order](test/zorder.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/test/zorder.clj))
+
+;; ## VR
+
+;; - ❌ [Surface](vr/surface.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/vr/surface.clj))
+;; - ❌ [Vector](vr/vector.html) ([code](https://github.com/mentat-collective/mathbox.cljs/tree/main/dev/mathbox/examples/vr/vector.clj))
